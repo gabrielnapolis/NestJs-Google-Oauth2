172f6d71e39f7e0fc0b0f9bcf042c40afdc26c1c
Gabriel Napolis <gabrielnapolis1604@gmail.com>
2024-06-29 23:51:07 -0300

    auth-service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,20 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { ConfigModule } from '@nestjs/config';
 import { GoogleStrategy } from './utils/GoogleStrategy';
+import { AuthService } from './auth.service';
+import { User } from 'src/typeorm/entities/User';
 
 @Module({
-    controllers: [AuthController],
-    providers: [GoogleStrategy],
-    imports: [ConfigModule.forRoot()],
+  controllers: [AuthController],
+  providers: [
+    GoogleStrategy,
+    {
+      provide: 'AUTH_SERVICE',
+      useClass: AuthService,
+    },
+  ],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forFeature([User])],
 })
 export class AuthModule {}
